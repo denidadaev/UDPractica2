eb01453e35840505238357cbadd3c307163f8d88
denidadaev <165592822+denidadaev@users.noreply.github.com>
2025-10-01 15:57:47 +0200

    CilindroVolumen.java

--- src/main/java/com/ieschabas/CilindroVolumen.java
+++ src/main/java/com/ieschabas/CilindroVolumen.java
@@ -6,12 +6,15 @@ public class CilindroVolumen {
     public static void main(String[] args) {
             Scanner scanner = new Scanner(System.in);
 
+            //Creo el radio del cilindro
             System.out.print("Introduce el radio: ");
             double radio = scanner.nextDouble();
 
+            //Introduzco la altura del cilindro
             System.out.print("Introduce la altura: ");
             double altura = scanner.nextDouble();
 
+            //Calculo el volumen segun la formula
             double volumen = Math.PI * Math.pow(radio, 2) * altura;
             System.out.println("Volumen: " + volumen);
 
