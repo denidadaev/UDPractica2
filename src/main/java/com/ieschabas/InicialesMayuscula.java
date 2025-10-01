3b289579a51abb71665f6444ca2f2b401676f545
denidadaev <165592822+denidadaev@users.noreply.github.com>
2025-10-01 15:59:20 +0200

    InicialesMayuscula.java

--- src/main/java/com/ieschabas/InicialesMayuscula.java
+++ src/main/java/com/ieschabas/InicialesMayuscula.java
@@ -6,14 +6,18 @@ public class InicialesMayuscula {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        //Introduzco el nombre
         System.out.print("Introduce tu nombre: ");
         String nombre = scanner.nextLine();
 
+        //Introduzco el apellido
         System.out.print("Introduce tu primer apellido: ");
         String apellido = scanner.nextLine();
 
+        //Saco las iniciales en mayuscula
         String initials = nombre.substring(0, 1).toUpperCase() + "." + apellido.substring(0, 1).toUpperCase() + ".";
 
+        //Las saco en la consola
         System.out.println("Iniciales: " + initials);
 
         scanner.close();
