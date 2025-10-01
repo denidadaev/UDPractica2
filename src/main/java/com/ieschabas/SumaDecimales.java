1206297759fc7172d616c1c712ee0aa86f1d9a5a
denidadaev <165592822+denidadaev@users.noreply.github.com>
2025-10-01 16:35:26 +0200

    SumaNotas.java

--- src/main/java/com/ieschabas/SumaDecimales.java
+++ src/main/java/com/ieschabas/SumaDecimales.java
@@ -2,24 +2,27 @@ package com.ieschabas;
 
 import java.util.Scanner;
 
-public class SumaDecimales {
+public class SumaNotas {
     public static void main(String[] args) {
         // Crear objeto Scanner
         Scanner scanner = new Scanner(System.in);
 
-        // Pedir el primer número
-        System.out.println("Introduce el primer número: ");
-        double numero1 = scanner.nextDouble();
+        // Pedir la primera nota
+        System.out.println("Introduce la primera nota: ");
+        double nota1 = scanner.nextDouble();
 
-        // Pedir el segundo número
-        System.out.println("Introduce el segundo número: ");
-        double numero2 = scanner.nextDouble();
+        // Pedir la segunda nota
+        System.out.println("Introduce la segunda nota: ");
+        double nota2 = scanner.nextDouble();
 
+        //Pedir la tercera nota
+        System.out.println("Introduce la tercera nota: ");
+        double nota3 = scanner.nextDouble();
         // Calcular la suma
-        double  suma = numero1 + numero2;
+        double  suma = nota1 + nota2 + nota3;
 
         // Mostrar la suma en el formato solicitado
-        System.out.println(numero1 + " + " + numero2 + " = " + suma);
+        System.out.println(nota1 + " + " + nota2 + "+" + nota3 + " = " + suma);
 
         // Cerrar el Scanner
         scanner.close();
