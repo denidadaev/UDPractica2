b49c2707d603e7f7f56b6512bb050056c36386df
denidadaev <165592822+denidadaev@users.noreply.github.com>
2025-10-01 16:09:50 +0200

    MinutosASegundos.java

--- src/main/java/com/ieschabas/MinutosASegundos.java
+++ src/main/java/com/ieschabas/MinutosASegundos.java
@@ -6,25 +6,29 @@ public class MinutosASegundos {
         public static void main(String[] args) {
             Scanner scanner = new Scanner(System.in);
 
+            //Introduzco los minutos de la primera canción    
             System.out.print("Introduce los minutos de la primera canción: ");
-            int minutes1 = scanner.nextInt();
+            int minutos1 = scanner.nextInt();
 
+            //Segundos de la primera cancion    
             System.out.print("Introduce los segundos de la primera canción: ");
-            int seconds1 = scanner.nextInt();
+            int segundos1 = scanner.nextInt();
 
+            //Minutos de la segunda cancion
             System.out.print("Introduce los minutos de la segunda canción: ");
-            int minutes2 = scanner.nextInt();
+            int minutos2 = scanner.nextInt();
 
+            //Segundos de la segunda cancion    
             System.out.print("Introduce los segundos de la segunda canción: ");
-            int seconds2 = scanner.nextInt();
+            int segundos2 = scanner.nextInt();
 
-            int totalSeconds = (minutes1 * 60 + seconds1) + (minutes2 * 60 + seconds2);
-            int totalMinutes = totalSeconds / 60;
-            int remainingSeconds = totalSeconds % 60;
+            //Traduzco los segundos en minutos y le pongo el resto de segundos a través del módulo    
+            int totalSegundos = (minutos1 * 60 + segundos1) + (minutos2 * 60 + segundos2);
+            int totalMinutos = totalSegundos / 60;
+            int ElrestodeSegundos = totalSegundos % 60;
 
-            System.out.printf("Duración total: %d:%02d\n", totalMinutes, remainingSeconds);
+            System.out.println("Duración total: " + totalMinutos + ":" + ElrestodeSegundos);
 
             scanner.close();
         }
 }
-
