ef5f69ce934cd4f423e4529e4009b120ef07b16b
denidadaev <165592822+denidadaev@users.noreply.github.com>
2025-10-01 15:54:40 +0200

    Rectangulo.java

--- src/main/java/com/ieschabas/Rectangulo.java
+++ src/main/java/com/ieschabas/Rectangulo.java
@@ -5,16 +5,20 @@ import java.util.Scanner;
 public class Rectangulo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
+        //creo la base del rectangulo
         System.out.print("Introduce la base: ");
         double base = scanner.nextDouble();
-
+        
+        //Creo la altura del rectangulo
         System.out.print("Introduce la altura: ");
         double height = scanner.nextDouble();
 
+        //calculo el area y le perimetro del rectangulo mediante sus formulas
         double area = base * height;
         double perimeter = 2 * (base + height);
 
+        //las saco en la consola
         System.out.println("Área: " + area);
         System.out.println("Perímetro: " + perimeter);
 
