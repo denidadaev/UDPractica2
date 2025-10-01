7c6e103bb210b25d87eca97d8b51e1a79185f5e8
denidadaev <165592822+denidadaev@users.noreply.github.com>
2025-10-01 15:56:30 +0200

    FechaConCeros.java

--- src/main/java/com/ieschabas/FechaConCeros.java
+++ src/main/java/com/ieschabas/FechaConCeros.java
@@ -5,16 +5,20 @@ package com.ieschabas;
     public class FechaConCeros {
         public static void main(String[] args) {
             Scanner scanner = new Scanner(System.in);
-
+            
+            //Introduzco el dia
             System.out.print("Introduce el día: ");
             int dia = scanner.nextInt();
 
+            //Introduzco el mes
             System.out.print("Introduce el mes: ");
             int mes = scanner.nextInt();
 
+            //Introduzco el año
             System.out.print("Introduce el año: ");
             int año = scanner.nextInt();
 
+            //Los saco en la consola con guiones
             System.out.println("Fecha: " + año + "-" + mes + "-" + dia);
 
             scanner.close();
